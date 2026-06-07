ec9bb804166c19fd661aeafc7143fcc7059c3a9a
blxcode <bloxdmasterbloxdacc@gmail.com>
2026-06-07 16:11:05 -0700

    added textures

---
+++ script.js
@@ -0,0 +1,10 @@
+    function thing () {
+        let e = 67
+    }
+    // console.log(e) Will be undefined
+
+    function thing2 () {
+        var e = 67
+    
+    }
+    // console.log(e) Will be 67
